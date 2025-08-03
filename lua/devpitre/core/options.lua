75e31f35d36deac6c6ea71e785f79d0a2be95413
CARLOS <carlos@MacBook-Pro-de-CARLOS.local>
2025-08-02 21:01:27 -0500

    feat: add which-key

--- lua/devpitre/core/options.lua
+++ lua/devpitre/core/options.lua
@@ -1,12 +1,11 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
-
 local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
 opt.expandtab = true
-opt.autoindent = true 
+opt.autoindent = true
 opt.tabstop = 2
 opt.shiftwidth = 2
 
@@ -15,11 +14,9 @@ opt.wrap = false
 opt.ignorecase = true
 opt.smartcase = true
 
-
 opt.termguicolors = true
 opt.background = "dark"
 opt.signcolumn = "yes"
-
 
 opt.clipboard:append("unnamedplus")
 
--- lua/devpitre/plugins/which-key.lua
+++ lua/devpitre/plugins/which-key.lua
@@ -1,13 +1,52 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	config = function()
+		local wk = require("which-key")
+		wk.add({
+			{ "<leader>f", group = "file", desc = "File" }, -- group
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+			{
+				"<leader>fb",
+				function()
+					print("hello")
+				end,
+				desc = "Foobar",
+			},
+			{ "<leader>fn", desc = "New File" },
+			{ "<leader>f1", hidden = true }, -- hide this keymap
+			{ "<leader>w", proxy = "<c-w>", group = "windows" }, -- proxy to window mappings
+			{
+				"<leader>b",
+				group = "buffers",
+				expand = function()
+					return require("which-key.extras").expand.buf()
+				end,
+			},
+			{
+				-- Nested mappings are allowed and can be added in any order
+				-- Most attributes can be inherited or overridden on any level
+				-- There's no limit to the depth of nesting
+				mode = { "n", "v" }, -- NORMAL and VISUAL mode
+				{ "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+				{ "<leader>w", "<cmd>w<cr>", desc = "Write" },
+			},
+		})
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	-- groups = {
+	-- 	n = {
+	-- 		["<leader>"] = {
+	-- 			c = { name = "code" },
+	-- 		},
+	-- 	},
+	-- },
 }
